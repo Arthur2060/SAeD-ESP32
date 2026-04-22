f8d0db5f1e73d8301b48df71bcf5b7d07fdc6d3f
Arthur <arthurhenrrique1705@gmail.com>
2026-04-22 17:44:39 -0300

    Primeira versão para testes pronta

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
-#include "model/Bussola.hpp"
-#include "model/Ultrasom.hpp"
+#include "controll/Radar.hpp"
+
+Radar radar;
 
 void setup() {
-
+  Serial.begin(9600);
+  radar.begin(14, 13);
 }
 
 void loop() {
-
+  float leitura[2] = {radar.getObstacle()[0], radar.getObstacle()[1]};
+  Serial.printf("X = %.2fm, Y = %.2fm\n", leitura[0], leitura[1]);
+  delay(1000);
 }
