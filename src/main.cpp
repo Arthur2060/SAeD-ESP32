3a3b945deefb801bb4836053a0f877d599b3b8b9
Arthur <arthurhenrrique1705@gmail.com>
2026-04-22 10:57:47 -0300

    Apagando projeto antigo e recomeçando descartando ROS por excesso de complexidade e falta de tempo

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,10 @@
-#include <Arduino.h>
-#include "ros2arduino.h"
-#include "Ultrasom.hpp"
 #include "Bussola.hpp"
-#include <vector>
-
-std::vector<char> commands;
-
-void subscribeCommand(std_msgs::String* msg, void* args) {
-  (void)(args);
-
-  for (char command : msg->data) {
-    commands.push_back(command);
-  }
-}
-
-class CommandSub : public ros2::Node 
-{
-  public:
-    CommandSub() : Node("command_sub")
-    {
-      ros2::Subscriber<std_msgs::String>* subscriber_ = 
-      this->createSubscriber<std_msgs::String>("commands", (ros2::CallbackFunc) subscribeCommand, nullptr);
-    }
-
-  private:
-};
-
-Bussola bussola;
-Ultrasom ultrasom(14, 12); // Pino 14 - Trigger, Pino 12 - Echo
+#include "Ultrasom.hpp"
 
 void setup() {
-    Serial.begin(9600);
+
 }
 
 void loop() {
-    static CommandSub command;
 
-    int distance = ultrasom.collectUltrasonicData();
-    int angle = bussola.collectCompassData();
-    
-    Serial.printf("Distance: %i, Angle: %.2f\n", distance, angle);
-    ros2::spin(&command);
 }
