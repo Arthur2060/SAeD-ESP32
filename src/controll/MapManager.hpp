5ae8912c6b26e5c28d333fdb92cfa3f230fd7060
Arthur <arthurhenrrique1705@gmail.com>
2026-04-24 23:50:53 -0300

    Começando classe para comunicação WebSocket para IHM

--- src/controll/MapManager.hpp
+++ src/controll/MapManager.hpp
@@ -1,14 +1,16 @@
 #include <vector>
 #include "Demarcacao.hpp"
 
-class SAeDMap {
+class MapManager {
     private:
         std::vector<std::vector<bool>> map;
         float distanciaDeQuadro = 0.3f;
         Demarcacao demarcacao;
     
     public:
-        SAeDMap(int scalex, int scaley, float distanciaDeQuadro) {
+        MapManager() {}
+
+        MapManager(int scalex, int scaley, float distanciaDeQuadro) {
             for (int x = 0 ; x <= scalex ; x++) {
                 for (int y = 0 ; y <= scaley ; y++) {
                     this->map[x].push_back(false);
--- src/view/WebIHM.hpp
+++ src/view/WebIHM.hpp
@@ -1,6 +1,21 @@
+#include "ESPAsyncWebServer.h"
+#include "AsyncTCP.h"
+#include "ArduinoJson.h"
+#include "controll/MapManager.hpp"
+
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
+
 class WebIHM {
     private:
-        
-    public:
+        MapManager mapManager;
 
+    public:
+        WebIHM(MapManager MapManager) {
+            this->mapManager = mapManager;
+        }
+
+        void begin() {
+            server.addHandler(&ws);
+        }
 };
