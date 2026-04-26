e38e4d4321cfd17483a3dfa3665d1ca0c603489a
Arthur <arthurhenrrique1705@gmail.com>
2026-04-25 21:15:28 -0300

    Adicionando conexão Web Socket

--- src/view/WebIHM.hpp
+++ src/view/WebIHM.hpp
@@ -2,6 +2,7 @@
 #include "AsyncTCP.h"
 #include "ArduinoJson.h"
 #include "controll/MapManager.hpp"
+#include <WiFi.h>
 
 AsyncWebServer server(80);
 AsyncWebSocket ws("/ws");
@@ -9,13 +10,38 @@ AsyncWebSocket ws("/ws");
 class WebIHM {
     private:
         MapManager mapManager;
+        IPAddress ip;
 
     public:
         WebIHM(MapManager MapManager) {
             this->mapManager = mapManager;
         }
 
-        void begin() {
+        void begin(char* ssid, char* password) {
+            WiFi.begin(ssid, password);
+
+            while (WiFi.status() != WL_CONNECTED) {
+                delay(300);
+            }
+
+            ip = WiFi.localIP();
+
             server.addHandler(&ws);
+
+            server.begin();
+        }
+
+        // Retorna o IP do servidor WebSocket
+        IPAddress getIp() {
+            return this->ip;
+        }
+
+        // Envia algum contêudo no formato JSON para todos os clientes conectados
+        void sendSomething(JsonDocument content) {
+            String payload;
+
+            serializeJson(content, payload);
+
+            ws.textAll(payload);
         }
 };
