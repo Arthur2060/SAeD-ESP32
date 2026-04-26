3e78a93b4fbd626c39ed71409b5eccf80e0f994e
Arthur <arthurhenrrique1705@gmail.com>
2026-04-26 17:48:08 -0300

    Limpando main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,3 @@
-#include "model/Color.hpp"
+void setup() {}
 
-Color color;
-
-void setup() {
-  Serial.begin(9600);
-  color.begin();
-}
-
-void loop() {
-  std::vector<uint> valores = color.readColor();
-
-  Serial.printf("R=%i, G=%i, B=%i, Luminosidade=%i", valores[0], valores[1], valores[2], valores[3]);
-  delay(100);
-}
+void loop() {}
