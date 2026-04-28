b1bf3f7340d32525031ef7e48227c9d5c3ca8fa4
Arthur <arthurhenrrique1705@gmail.com>
2026-04-28 11:45:42 -0300

    Refatorando MapManager

--- src/controll/MapManager.hpp
+++ src/controll/MapManager.hpp
@@ -2,109 +2,147 @@
 #include "Demarcacao.hpp"
 #include "PathCalc.hpp"
 
-class MapManager {
-    private:
-        std::vector<std::vector<bool>> map;
-        float distanciaDeQuadro = 0.3f;
-        Demarcacao demarcacao;
-        PathCalc pathCalc;
-    
-    public:
-        MapManager() {}
+class MapManager
+{
+private:
+    std::vector<std::vector<bool>> map;
+    float distanciaDeQuadro = 0.3f;
+    Demarcacao demarcacao;
+    PathCalc pathCalc;
 
-        MapManager(int scaleX, int scaleY, float distanciaDeQuadro) {
-            for (int x = 0; x <= scaleX; x++)
-            {
-                map.push_back({});
-                std::vector<bool> targetLine = map[x];
-                for (int y = 0; y <= scaleY; y++)
-                {
-                    map[x].push_back(false);
-                }
-            }
+public:
+    MapManager() {}
 
-            this->distanciaDeQuadro = distanciaDeQuadro;
-            this->demarcacao = Demarcacao(map);
-            this->pathCalc = PathCalc(map);
-        }
-
-        MapManager(std::vector<std::vector<bool>> map, float distanciaDeQuadro) {
-            this->map = map;
-            this->distanciaDeQuadro = distanciaDeQuadro;
-            this->demarcacao = Demarcacao(map);
-        }
-        
-        float getDistanciaDeQuadro() {
-            return distanciaDeQuadro;
-        } 
-
-        float setDistanciaDeQuadro(float distancia) {
-            if (distancia > 0) {
-                distanciaDeQuadro = distancia;
-            }
-        }
-
-        bool addObstacle(float distanceX, float distanceY) {
-            float x = distanceX / distanciaDeQuadro;
-            float y = distanceY / distanciaDeQuadro;
-
-            if (!map[x][y]) {
-                map[x][y] = !map[x][y];
-                return true;
-            }
-
-            return false;
-        }
-
-        Demarcacao getDemarcacao() {
-            return this->demarcacao;
-        }
-
-        std::vector<std::vector<bool>> getMap() {
-            return this->map;
-        }
-
-        void setNewMap(int scalex, int scaley) {
-            for (int x = 0 ; x <= scalex ; x++) {
-                for (int y = 0 ; y <= scaley ; y++) {
-                    this->map[x].push_back(false);
-                }
-            }
-
-            this->demarcacao;
-        }
-
-        void setNewMap(std::vector<std::vector<bool>> map) {
-            this->map = map;
-            this->demarcacao.setMap(this->map);
-        }
-
-        void setNewMap(int scalex, int scaley) {
-            for (int x = 0 ; x <= scalex ; x++) {
-                for (int y = 0 ; y <= scaley ; y++) {
-                    this->map[x].push_back(false);
-                }
-            }
-            
-            this->demarcacao.setMap(this->map);
-        }
-
-        void setNewMap(int* scale) {
-            for (int x = 0 ; x <= scale[0] ; x++) {
-                for (int y = 0 ; y <= scale[1] ; y++) {
-                    this->map[x].push_back(false);
-                }
-            }
-
-            this->demarcacao.setMap(this->map);
-        }
-
-        std::vector<char> setTarget(int x, int y) {
-            return pathCalc.setTarget(x, y);
-        }
-
-        std::vector<char> setTarget(int *coordinates)
+    MapManager(int scaleX, int scaleY, float distanciaDeQuadro)
+    {
+        for (int x = 0; x <= scaleX; x++)
         {
-            return pathCalc.setTarget(coordinates);
+            map.push_back({});
+            std::vector<bool> targetLine = map[x];
+            for (int y = 0; y <= scaleY; y++)
+            {
+                map[x].push_back(false);
+            }
         }
-    };
+
+        this->distanciaDeQuadro = distanciaDeQuadro;
+        this->demarcacao = Demarcacao(map);
+        this->pathCalc = PathCalc(map);
+    }
+
+    MapManager(std::vector<std::vector<bool>> map, float distanciaDeQuadro)
+    {
+        this->map = map;
+        this->distanciaDeQuadro = distanciaDeQuadro;
+        this->demarcacao = Demarcacao(map);
+    }
+
+    float getDistanciaDeQuadro();
+    float setDistanciaDeQuadro(float distancia);
+
+    bool addObstacle(float distanceX, float distanceY);
+
+    Demarcacao getDemarcacao();
+
+    std::vector<std::vector<bool>> getMap();
+
+    void setNewMap(int scalex, int scaley);
+    void setNewMap(std::vector<std::vector<bool>> map);
+    void setNewMap(int scalex, int scaley);
+    void setNewMap(int *scale);
+
+    std::vector<char> setTarget(int x, int y);
+    std::vector<char> setTarget(int *coordinates);
+};
+
+float MapManager::getDistanciaDeQuadro()
+{
+    return distanciaDeQuadro;
+}
+
+float MapManager::setDistanciaDeQuadro(float distancia)
+{
+    if (distancia > 0)
+    {
+        distanciaDeQuadro = distancia;
+    }
+}
+
+bool MapManager::addObstacle(float distanceX, float distanceY)
+{
+    float x = distanceX / distanciaDeQuadro;
+    float y = distanceY / distanciaDeQuadro;
+
+    if (!map[x][y])
+    {
+        map[x][y] = !map[x][y];
+        return true;
+    }
+
+    return false;
+}
+
+Demarcacao MapManager::getDemarcacao()
+{
+    return this->demarcacao;
+}
+
+std::vector<std::vector<bool>> MapManager::getMap()
+{
+    return this->map;
+}
+
+void MapManager::setNewMap(int scalex, int scaley)
+{
+    for (int x = 0; x <= scalex; x++)
+    {
+        for (int y = 0; y <= scaley; y++)
+        {
+            this->map[x].push_back(false);
+        }
+    }
+
+    this->demarcacao;
+}
+
+void MapManager::setNewMap(std::vector<std::vector<bool>> map)
+{
+    this->map = map;
+    this->demarcacao.setMap(this->map);
+}
+
+void MapManager::setNewMap(int scalex, int scaley)
+{
+    for (int x = 0; x <= scalex; x++)
+    {
+        for (int y = 0; y <= scaley; y++)
+        {
+            this->map[x].push_back(false);
+        }
+    }
+
+    this->demarcacao.setMap(this->map);
+}
+
+void MapManager::setNewMap(int *scale)
+{
+    for (int x = 0; x <= scale[0]; x++)
+    {
+        for (int y = 0; y <= scale[1]; y++)
+        {
+            this->map[x].push_back(false);
+        }
+    }
+
+    this->demarcacao.setMap(this->map);
+}
+
+std::vector<char> MapManager::setTarget(int x, int y)
+{
+    return pathCalc.setTarget(x, y);
+}
+
+std::vector<char> MapManager::setTarget(int *coordinates)
+{
+    return pathCalc.setTarget(coordinates);
+}
