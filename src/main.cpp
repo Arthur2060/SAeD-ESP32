9d031dd26fe5d12b3ac5cd1c9b5cad59733de1fc
Arthur <arthurhenrrique1705@gmail.com>
2026-04-29 10:52:07 -0300

    Preparando Main para teste de Dual Core

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,34 @@
-void setup() {}
+#include <Arduino.h>
+#include "view/SAeD.hpp"
 
-void loop() {}
+TaskHandle_t Interno;
+
+SAeD saed;
+
+
+void setup() {
+    Serial.begin(9600);
+    saed.begin();
+
+    Serial.printf("Iniciando processamento em Core %i", xPortGetCoreID);
+}
+
+void loop() {
+    xTaskCreatePinnedToCore(
+        loop2, /* Function to implement the task */
+        "Task1",   /* Name of the task */
+        10000,     /* Stack size in words */
+        NULL,      /* Task input parameter */
+        0,         /* Priority of the task */
+        &Interno,    /* Task handle. */
+        0);        /* Core where the task should run */
+
+    saed.principalLoop();
+}
+
+void loop2( void * pvParameters) {
+    Serial.printf("Iniciando processamento em Core %i", xPortGetCoreID);
+    for(;;) {
+        saed.secondaryLoop();
+    }
+}
