5e557aaef8daeb389d44ea06aba02ef6959bbc33
Arthur <arthurhenrrique1705@gmail.com>
2026-05-29 09:03:51 -0300

    Limpando main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,9 @@
 #include <Arduino.h>
-#include <Wire.h>
-#include "model/Bussola.h"
-
-Bussola bussola;
 
 void setup()
 {
-  Serial.begin(9600);
-  bussola.begin();
 }
 
 void loop()
 {
-  Serial.println(bussola.collectCompassData());
 }
